514db64e44c803fdc92725852b87f8550a2fa467
Dipak Dandge <dipakdandge256@gmail.com>
2025-09-02 12:52:47 +0530

    change route

--- src/main/java/com/dipak/controller/WelcomeRestController.java
+++ src/main/java/com/dipak/controller/WelcomeRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeRestController {
 	
-	@GetMapping("/welcome")
+	@GetMapping("/")
 	public String getWelcome() {
 		return "Welcome to coding universe";
 	}
