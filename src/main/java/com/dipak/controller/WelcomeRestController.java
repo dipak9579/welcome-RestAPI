e2ff661f2a0fc8573e9c83a68483e59df85908b9
Dipak Dandge <dipakdandge256@gmail.com>
2025-09-02 11:54:52 +0530

    first commit

---
+++ src/main/java/com/dipak/controller/WelcomeRestController.java
@@ -0,0 +1,14 @@
+package com.dipak.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@GetMapping("/welcome")
+	public String getWelcome() {
+		return "Welcome to coding universe";
+	}
+
+}
